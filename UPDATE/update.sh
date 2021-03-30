0ee7debf4007af69622205dbe893702f715a7d1f
Prince Gutierrez <81551746+PRINCEGUT-12@users.noreply.github.com>
2021-03-30 22:35:13 +0800

    Create update.sh

---
+++ UPDATE/update.sh
@@ -0,0 +1,15 @@
+#tools by prince gutierrez
+#run this code without error
+clear
+echo
+printf "\e[1;40m UPDATING NGROK......\e[0m\n"
+sleep 1
+https://github.com/PRINCEGUT-12/NGROK
+echo
+clear
+cd $HOME
+cd $HOME/NGROK/UPDATE
+printf "\e[1;40m NGROK SUCCESSFULLY UPDATE\e[0m\n"
+   fi
+cd $HOME/NGROK
+bash ngrok.sh
