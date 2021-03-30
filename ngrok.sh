8ff0d5e90dec69c13c2c3f519cd39d60e1fbaf82
Prince Gutierrez <81551746+PRINCEGUT-12@users.noreply.github.com>
2021-03-31 00:20:52 +0800

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -49,24 +49,6 @@ exit
    fi
 }
 
-download_ngrok() {
-	url="$1"
-	file=`basename $url`
-	if [[ -e "$file" ]]; then
-		rm -rf "$file"
-	fi
-	wget --no-check-certificate "$url" > /dev/null 2>&1
-	if [[ -e "$file" ]]; then
-		unzip "$file" > /dev/null 2>&1
-		mv -f ngrok .server/ngrok > /dev/null 2>&1
-		rm -rf "$file" > /dev/null 2>&1
-		chmod +x .server/ngrok > /dev/null 2>&1
-	else
-		echo -e $red Error occured, Install Ngrok manually."
-		{ reset_color; exit 1; }
-	fi
-}
-
 install_ngrok() {
 	if [[ -e ".server/ngrok" ]]; then
 		echo -e $red Ngrok already installed."
@@ -84,6 +66,24 @@ install_ngrok() {
 		fi
 	fi
 
+}
+
+download_ngrok() {
+	url="$1"
+	file=`basename $url`
+	if [[ -e "$file" ]]; then
+		rm -rf "$file"
+	fi
+	wget --no-check-certificate "$url" > /dev/null 2>&1
+	if [[ -e "$file" ]]; then
+		unzip "$file" > /dev/null 2>&1
+		mv -f ngrok .server/ngrok > /dev/null 2>&1
+		rm -rf "$file" > /dev/null 2>&1
+		chmod +x .server/ngrok > /dev/null 2>&1
+	else
+		echo -e $red Error occured, Install Ngrok manually."
+		{ reset_color; exit 1; }
+	fi
 }
 
 dependencies
